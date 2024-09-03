ba2f61ee2d98363f775e2c9536c28ad6eb45786d
FabioStorer <fabiostorer19@gmail.com>
2024-09-03 09:30:01 -0300

    feat: adicionado index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const aluguel_router = require('./routes/aluguel_router.js');
+const estudante_router = require('./routes/estudante_router.js');
+const livro_router = require('./routes/livro_router.js');
+const app = express();
+const port = 5000;
+
+app.use('/aluguel', aluguel_router);
+app.use('/estudante', estudante_router);
+app.use('/livro', livro_router);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}.`);
+});
